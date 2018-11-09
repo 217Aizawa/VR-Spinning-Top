1432a8ea56137bb2e80f0d9940a6e8a949415436
norm-ideal <idehara.n@gmail.com>
2018-11-09 17:51:56 +0900

    UDP Sender/Receiver components

---
+++ Assets/VRSpinningTop/Scripts/UDPReceiver.cs
@@ -0,0 +1,152 @@
+﻿/*
+ 
+    -----------------------
+    UDP-Receive (send to)
+    -----------------------
+    // [url]http://msdn.microsoft.com/de-de/library/bb979228.aspx#ID0E3BAC[/url]
+   
+   
+    // > receive
+    // 127.0.0.1 : 8051
+   
+    // send
+    // nc -u 127.0.0.1 8051
+ 
+*/
+using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UDPReceiver : MonoBehaviour
+{
+
+    // receiving Thread
+    Thread receiveThread;
+
+    // udpclient object
+    UdpClient client;
+
+    // public
+    private string IP;
+	public int port = 28080;
+	
+    // infos
+    public string lastReceivedUDPPacket = "";
+    // public string allReceivedUDPPackets = ""; // clean up this from time to time!
+
+    // start from shell
+    private static void Main()
+    {
+        UDPReceiver receiveObj = new UDPReceiver();
+        receiveObj.init();
+
+        string text = "";
+        do
+        {
+            text = Console.ReadLine();
+        }
+        while (!text.Equals("exit"));
+    }
+
+    // start from unity3d
+    public void Start()
+    {
+        init();
+    }
+
+    // OnGUI
+    void OnGUI()
+    {
+		Rect rectObj = new Rect(0, 0, 200, 400);
+        GUIStyle style = new GUIStyle();
+        style.alignment = TextAnchor.UpperLeft;
+        GUI.Box(rectObj, "# UDPReceive\n" + IP.ToString() + ":" + port + " #\n"
+                    + "shell> nc -u 127.0.0.1 : " + port + " \n"
+                    + "\nLast Packet: \n" + lastReceivedUDPPacket
+                , style);
+    }
+
+    // init
+    private void init()
+    {
+        // Endpunkt definieren, von dem die Nachrichten gesendet werden.
+        print("UDPReceiver.init()");
+
+		var host = Dns.GetHostEntry(Dns.GetHostName());
+
+		IP = "";
+		foreach (var ip in host.AddressList)
+		{
+			if (ip.AddressFamily == AddressFamily.InterNetwork)
+			{
+				Debug.Log("Found MyIP : " + ip.ToString());
+
+				if (IP.Length == 0)
+				{
+					IP = ip.ToString();
+					Debug.Log("MyIP : " + IP);
+				}
+
+			}
+		}
+
+        // ----------------------------
+        // Abhören
+        // ----------------------------
+        // Lokalen Endpunkt definieren (wo Nachrichten empfangen werden).
+        // Einen neuen Thread für den Empfang eingehender Nachrichten erstellen.
+        receiveThread = new Thread(new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+
+		Debug.Log("UDP Receiver Started");
+    }
+
+    // receive thread
+    private void ReceiveData()
+    {
+        client = new UdpClient(port);
+        while (true)
+        {
+            try
+            {
+                // Bytes empfangen.
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref anyIP);
+
+                // Bytes mit der UTF8-Kodierung in das Textformat kodieren.
+                string text = Encoding.UTF8.GetString(data);
+
+                // Den abgerufenen Text anzeigen.
+                // Debug.Log(">> " + text);
+
+                // latest UDPpacket
+                lastReceivedUDPPacket = text;
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+    // getLatestUDPPacket
+    // cleans up the rest
+    public string getLatestUDPPacket()
+    {
+        return lastReceivedUDPPacket;
+    }
+
+    void OnApplicationQuit()
+    {
+        if (receiveThread != null)
+            receiveThread.Abort();
+
+        client.Close();
+    }
+}
---
+++ Assets/VRSpinningTop/Scripts/UDPSender.cs
@@ -0,0 +1,212 @@
+﻿
+/*
+ 
+    -----------------------
+    UDP-Send
+    -----------------------
+    // [url]http://msdn.microsoft.com/de-de/library/bb979228.aspx#ID0E3BAC[/url]
+   
+    // > gesendetes unter
+    // 127.0.0.1 : 8050 empfangen
+   
+    // nc -lu 127.0.0.1 8050
+ 
+        // todo: shutdown thread at the end
+*/
+using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UDPSender : MonoBehaviour
+{
+    private static int localPort;
+
+	public bool DEBUG = false;
+
+    // prefs
+    private string IP;  // define in init
+	private IPAddress broadcast;
+    public int port = 28080;  // define in init
+
+	// sendout timer (to prevent too many packets)
+	float timer;
+
+    // "connection" things
+    IPEndPoint remoteEndPoint;
+    UdpClient client;
+
+    // gui
+    string strMessage = "";
+
+    // call it from shell (as program)
+    private static void Main()
+    {
+        UDPSender sendObj = new UDPSender();
+        sendObj.init();
+
+        // testing via console
+        // sendObj.inputFromConsole();
+
+        // as server sending endless
+        sendObj.sendEndless(" endless infos \n");
+
+    }
+    // start from unity3d
+    public void Start()
+    {
+        init();
+    }
+	
+	public void Update()
+	{
+		if (!DEBUG)
+			return;
+
+		timer += Time.deltaTime;
+		if (timer > 1.0f)
+		{
+			Debug.Log("test");
+			sendString("QTC");
+			timer = 0;
+		}
+	}
+
+    // OnGUI
+    void OnGUI()
+    {
+        Rect rectObj = new Rect(0, 0, 200, 400);
+        GUIStyle style = new GUIStyle();
+        style.alignment = TextAnchor.UpperLeft;
+        GUI.Box(rectObj, "# UDPSend-Data\n " + IP + ":" + port + " #\n"
+                    + "shell> nc -lu   " + IP + ":" + port + " \n"
+                , style);
+
+        // ------------------------
+        // send it
+        // ------------------------
+        strMessage = GUI.TextField(new Rect(0, 220, 140, 20), strMessage);
+        if (GUI.Button(new Rect(0, 250, 40, 20), "send"))
+        {
+            sendString(strMessage + "\n");
+        }
+    }
+
+    // init
+    public void init()
+    {
+        // Endpunkt definieren, von dem die Nachrichten gesendet werden.
+        print("UDPSend.init()");
+
+		var host = Dns.GetHostEntry(Dns.GetHostName());
+		IP = "";
+
+		foreach (var ip in host.AddressList)
+		{
+			if (ip.AddressFamily == AddressFamily.InterNetwork)
+			{
+				Debug.Log("Found MyIP : " + ip.ToString());
+
+				byte[] broadcastaddress = ip.GetAddressBytes();
+				broadcastaddress[3] = 255;  // assume 24 bit mask
+				if (IP.Length == 0)
+				{
+					broadcast = new IPAddress(broadcastaddress);
+					IP = broadcast.ToString();
+					Debug.Log("Broadcast to : " + IP);
+				}
+
+			}
+		}
+
+		// define
+		// IP = "127.0.0.1";
+        // defined on Editor
+        // port = 8051;
+
+        // ----------------------------
+        // Senden
+        // ----------------------------
+        remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), port);
+        client = new UdpClient();
+
+        // status
+        Debug.Log("Sending to " + IP + " : " + port);
+        Debug.Log("Testing: nc -lu " + IP + " : " + port);
+
+    }
+
+    // inputFromConsole
+    private void inputFromConsole()
+    {
+        try
+        {
+            string text;
+            do
+            {
+                text = Console.ReadLine();
+
+                // Den Text zum Remote-Client senden.
+                if (text != "")
+                {
+
+                    // Daten mit der UTF8-Kodierung in das Binärformat kodieren.
+                    byte[] data = Encoding.UTF8.GetBytes(text);
+
+                    // Den Text zum Remote-Client senden.
+                    client.Send(data, data.Length, remoteEndPoint);
+                }
+            } while (text != "");
+        }
+        catch (Exception err)
+        {
+            print(err.ToString());
+        }
+
+    }
+
+    // sendData
+    public void sendString(string message)
+    {
+        try
+        {
+            //if (message != "")
+            //{
+
+            // Daten mit der UTF8-Kodierung in das Binärformat kodieren.
+            byte[] data = Encoding.UTF8.GetBytes(message);
+
+            // Den message zum Remote-Client senden.
+            client.Send(data, data.Length, remoteEndPoint);
+            //}
+        }
+        catch (Exception err)
+        {
+            print(err.ToString());
+        }
+    }
+
+
+    // endless test
+    private void sendEndless(string testStr)
+    {
+        do
+        {
+            sendString(testStr);
+
+
+        }
+        while (true);
+
+    }
+
+    void OnApplicationQuit()
+    {
+        client.Close();
+    }
+
+}
