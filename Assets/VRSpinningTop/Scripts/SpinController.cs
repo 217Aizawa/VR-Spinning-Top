5ff7a584e52531aa426e75b2d252ee058749de9f
217Aizawa <21711001ha@tama.ac.jp.address>
2018-11-21 20:24:27 +0900

    fix effect

--- Assets/VRSpinningTop/Scripts/SpinController.cs
+++ Assets/VRSpinningTop/Scripts/SpinController.cs
@@ -1,42 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SpinController : MonoBehaviour {
-
-    public bool isThrown;//投げられたかの判定
-    public Vector3 velocity;//速度
-    public Vector3 Axis;//軸の向き
-
-	// Use this for initialization
-	void Start () {
-        ResetSpin();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown("space"))
-        {
-            velocity = new Vector3(0, 0, 15);//変数velocityにVector3構造体をセットする。
-            isThrown = true;
-        }
-
-        if(isThrown == true)
-        {
-            ForkParticlePlugin.Instance.Test();
-        }
-	}
-
-    public void ResetSpin()//初期化
-    {
-        isThrown = false;
-        velocity = Vector3.zero;//zero = (0, 0, 0)
-        Axis = Vector3.up;
-    }
-}
-=======
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -78,7 +40,10 @@ public class SpinController : MonoBehaviour
 
 
         //velocity = new Vector3(0, 0, -5);//変数velocityにVector3構造体をセットする。
-
+        if(isThrown == true)
+        {
+            ForkParticlePlugin.Instance.Test();
+        }
     }
 
     public void ResetSpin()//初期化
@@ -107,4 +72,3 @@ public class SpinController : MonoBehaviour
 
 
 }
->>>>>>> bd752cd898b7e50c5ecd14909cddc8f90e6bd57e
