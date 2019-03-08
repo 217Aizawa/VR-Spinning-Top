56f30954116656399ef8361ca0085e91362f321b
217Aizawa <21711001ha@tama.ac.jp.address>
2019-03-08 10:11:19 +0100

    success change camera

--- Assets/VRSpinningTop/Scripts/CameraController.cs
+++ Assets/VRSpinningTop/Scripts/CameraController.cs
@@ -4,14 +4,45 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Camera SubCamera;
+    private float cntTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Counter();
+        ChangeCamera();
+        /*FindKomaTag();
+        GameObject.FindGameObjectsWithTag("Koma");//Komaタグがついているオブジェクトを探す。*/
+    }
+
+    /*void FindKomaTag()
+    {
+        if (GameObject.FindGameObjectsWithTag("Koma").Length == 1)
+        {
+            GameObject.FindWithTag("Koma");
+        }
+    }*/
+    
+    void Counter()
+    {
+        if (SpinController.isThrown == true)
+        {
+            cntTime += Time.deltaTime;
+        }
+    }
+    void ChangeCamera()
+    {
+        if(SpinController.isThrown && cntTime > 3)
+        {
+            SubCamera.depth = 3;//カメラの深度を3プラスする
+
+        }
     }
 }
